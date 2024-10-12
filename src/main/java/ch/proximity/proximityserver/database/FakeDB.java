c5d258a93e4a99872d10d070753e7cc02fd3da0a
--replace-all <muchembled.martin@gmail.com>
2024-10-12 20:54:05 +0200

    feat: added test and main instances

--- src/main/java/ch/proximity/proximityserver/database/FakeDB.java
+++ src/main/java/ch/proximity/proximityserver/database/FakeDB.java
@@ -19,8 +19,14 @@ public class FakeDB implements DBReader, DBWriter {
     private final SimpleDirectedGraph<ProximityNode, ProximityEdge> graph;
     private final Map<String, ProximityNode> uuidToNode = new HashMap<>();
 
+    private static FakeDB mainInstance;
+
     public FakeDB(){
         this.graph = new SimpleDirectedGraph<>(ProximityEdge.class);
+    }
+
+    private FakeDB(SimpleDirectedGraph<ProximityNode, ProximityEdge> graph){
+        this.graph = graph;
     }
 
     /**
@@ -38,6 +44,7 @@ public class FakeDB implements DBReader, DBWriter {
 
         LinkedList<Pair<ProximityNode, Integer>> queue = new LinkedList<>();
         queue.add(new Pair<>(source, 0));
+        subGraph.addVertex(source);
 
         Pair<ProximityNode, Integer> current;
         while(!queue.isEmpty()){
@@ -77,8 +84,7 @@ public class FakeDB implements DBReader, DBWriter {
     }
 
     private ProximityNode nodeFromUUID(String uid){
-
-        return null;
+        return uuidToNode.get(uid);
     }
 
     private void acquireWriterLock(){
@@ -128,8 +134,68 @@ public class FakeDB implements DBReader, DBWriter {
 
     }
 
-    private SimpleDirectedGraph<ProximityNode, ProximityEdge> fakeInstance(){
-        return null;
+    public static FakeDB getInstance(){
+        return mainInstance;
+    }
+
+    public static FakeDB fakeInstance(){
+        ProximityNode[] nodes = {
+                new ProximityNode("1", "a1a"),
+                new ProximityNode("2", "a2a"),
+                new ProximityNode("3", "a3a"),
+                new ProximityNode("4", "a4a"),
+                new ProximityNode("5", "a5a"),
+                new ProximityNode("6", "a6a"),
+                new ProximityNode("7", "a7a"),
+                new ProximityNode("8", "a8a"),
+                new ProximityNode("9", "a9a"),
+                new ProximityNode("10", "a10a"),
+                new ProximityNode("11", "a11a"),
+                new ProximityNode("12", "a12a"),
+                new ProximityNode("13", "a13a"),
+                new ProximityNode("14", "a14a"),
+        };
+
+        ProximityEdge[] edges = {
+                new ProximityEdge("a", 20, "1", "2"),
+                new ProximityEdge("b", 20, "2", "1"),
+                new ProximityEdge("c", 20, "2", "3"),
+                new ProximityEdge("d", 20, "3", "2"),
+                new ProximityEdge("e", 30, "3", "14"),
+                new ProximityEdge("f", 30, "14", "3"),
+                new ProximityEdge("g", 30, "4", "14"),
+                new ProximityEdge("h", 30, "14", "4"),
+                new ProximityEdge("i", 40, "5", "14"),
+                new ProximityEdge("j", 40, "6", "14"),
+                new ProximityEdge("k", 40, "14", "6"),
+                new ProximityEdge("l", 40, "6", "7"),
+                new ProximityEdge("m", 50, "7", "6"),
+                new ProximityEdge("n", 50, "7", "9"),
+                new ProximityEdge("o", 50, "9", "7"),
+                new ProximityEdge("p", 50, "9", "10"),
+                new ProximityEdge("q", 60, "10", "9"),
+                new ProximityEdge("r", 60, "11", "14"),
+                new ProximityEdge("s", 60, "14", "11"),
+                new ProximityEdge("t", 60, "11", "12"),
+                new ProximityEdge("u", 60, "12", "11"),
+                new ProximityEdge("v", 60, "12", "13"),
+                new ProximityEdge("w", 60, "13", "12"),
+                new ProximityEdge("w", 70, "14", "9"),
+                new ProximityEdge("w", 70, "9", "14"),
+                new ProximityEdge("w", 70, "6", "9"),
+                new ProximityEdge("w", 70, "9", "6"),
+
+
+
+        };
+
+        SimpleDirectedGraph<ProximityNode, ProximityEdge> fakeGraph = new SimpleDirectedGraph<>(ProximityEdge.class);
+
+        FakeDB db = new FakeDB();
+        db.addNodes(List.of(nodes));
+        db.addEdges(List.of(edges));
+
+        return db;
     }
 
 }
