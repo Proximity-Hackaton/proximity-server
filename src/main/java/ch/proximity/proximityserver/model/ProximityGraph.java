acc2b99b598152eab6088ad1fd548c175c6db4e4
--replace-all <muchembled.martin@gmail.com>
2024-10-12 20:55:28 +0200

    Feat: implemented graph model

--- src/main/java/ch/proximity/proximityserver/model/ProximityGraph.java
+++ src/main/java/ch/proximity/proximityserver/model/ProximityGraph.java
@@ -1,4 +1,28 @@
 package ch.proximity.proximityserver.model;
 
 public class ProximityGraph {
+
+    private ProximityEdge[] edges;
+    private ProximityNode[] nodes;
+
+    public ProximityGraph(ProximityEdge[] edges, ProximityNode[] nodes) {
+        this.edges = edges;
+        this.nodes = nodes;
+    }
+
+    public ProximityEdge[] getEdges() {
+        return edges;
+    }
+
+    public void setEdges(ProximityEdge[] edges) {
+        this.edges = edges;
+    }
+
+    public ProximityNode[] getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(ProximityNode[] nodes) {
+        this.nodes = nodes;
+    }
 }
