6ce026796ab37b18cf11b72f6aa26be12f98fb66
--replace-all <muchembled.martin@gmail.com>
2024-10-12 10:22:01 +0200

    First commit

---
+++ src/main/java/ch/muchembled/martin/proximityserver/ProximityServerApplication.java
@@ -0,0 +1,13 @@
+package ch.muchembled.martin.proximityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProximityServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/muchembled/martin/proximityserver/ProximityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.muchembled.martin.proximityserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximityServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
