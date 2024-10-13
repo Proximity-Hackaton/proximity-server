5e37acbc2d2388bab134b81c5d0cda6f7a5d0cf6
--replace-all <muchembled.martin@gmail.com>
2024-10-13 04:55:57 +0200

    feat: main function

--- src/main/java/ch/proximity/proximityserver/ProximityServerApplication.java
+++ src/main/java/ch/proximity/proximityserver/ProximityServerApplication.java
@@ -1,5 +1,9 @@
 package ch.proximity.proximityserver;
 
+import ch.proximity.proximityserver.database.DBWriter;
+import ch.proximity.proximityserver.database.FakeDB;
+import ch.proximity.proximityserver.suipoller.EventPoller;
+import ch.proximity.proximityserver.suipoller.EventPollerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProximityServerApplication {
 
     public static void main(String[] args) {
+        EventPoller eventPoller = new EventPollerImpl();
+        DBWriter db = FakeDB.getInstance();
+        eventPoller.registerOnNewNodes((ns) -> db.addNodes(ns));
+        eventPoller.registerOnNewEdges((es) -> db.addEdges(es));
+        eventPoller.start();
         SpringApplication.run(ProximityServerApplication.class, args);
+        eventPoller.stop();
     }
 
 }
